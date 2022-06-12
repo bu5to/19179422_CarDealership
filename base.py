d42cbfd23e7a31bca6f5dcc46f1555571c9ba4f2
Busto <33113637+bu5to@users.noreply.github.com>
2022-06-13 00:45:44 +0200

    Registering new user

---
+++ base.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+# The database is initialised.
+engine = create_engine(os.environ.get('SQL_DATABASE'))
+Session = sessionmaker(bind=engine)
+Session.expire_on_commit = False
+
+Base = declarative_base()
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+from base import Session, engine, Base
+from models import User, Car
 
 import base64
 
@@ -11,7 +13,14 @@ def index():
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        print(request.form.get("name"))
+        new_user = User(request.form["username"],request.form["name"],request.form["email"],
+                        request.form["password"],request.form["role"],request.form["address"],
+                        request.form["profilePic"],request.form["phone"])
+        session.add(new_user)
+        session.commit()
+        session.expunge_all()
+        session.close()
+        return redirect(url_for('login'))
     return render_template('register.html')
 
 @app.route('/login', methods=["GET", "POST"])
